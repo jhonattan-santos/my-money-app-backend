e210e2001086c5f0a00186cee24928f07bcba251
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2020-03-24 09:19:13 -0300

    changing enviroments

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const env = require('../.env')
+const env = process.env.AUTH_SECRET ? process.env.AUTH_SECRET : require('../.env')
 
 module.exports = (req, res, next) => {
     // CORS preflight request
