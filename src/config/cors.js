9045109f598cf3557f21026f2e14da59a3c6391f
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2020-07-19 21:05:03 -0300

    bugfix cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
-module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://mm-app-front-react.herokuapp.com')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
